644f9f23a5f7e8c71c2b01b37032a03003826268
meh <103293269+mekelcruzz@users.noreply.github.com>
2024-12-12 12:59:56 +0800

    henlin

--- backend/app.py
+++ backend/app.py
@@ -673,5 +673,5 @@ def analyze_sentiment():
 
 
 if __name__ == "__main__":
-     port = int(os.environ.get("PORT", 5000))
+     port = int(os.environ.get("PORT_APP", 5001))  # Default to 5001 for local
      app.run(debug=True, host="0.0.0.0", port=port)
